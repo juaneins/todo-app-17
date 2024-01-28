0c6b5c1f0d91f1e91d5b099980468df1a9e32aa4
juaneins_uio <ballesterosjg@gmail.com>
2024-01-27 19:52:49 -0500

    labs signal

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,5 +11,36 @@ import { CommonModule } from '@angular/common';
 export class LabsComponent {
   gretting = 'Hola';
 
-  tasks = ['install angular cli', 'create project', 'create components'];
+  tasks = signal([
+    'install angular cli',
+    'create project',
+    'create components',
+    'create service',
+  ]);
+
+  name = signal('Juan');
+  age = 45;
+  disabled = false;
+  imgSrc = 'https://www.w3schools.com/howto/img_avatar.png';
+
+  person = {
+    name: 'Juan',
+    age: 45,
+    avatar: 'https://www.w3schools.com/howto/img_avatar.png',
+  };
+
+  clickHandler() {
+    console.log('click');
+  }
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+
+  changeKeyHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
